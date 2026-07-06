1779a9bd8e3251a101602e43a7a57fbe61d88510
ValeriiSlynko <valera.slinko@gmail.com>
2026-07-07 00:00:46 +0300

    Comp. Vis. (AI) CLASS Works - Images Filters (02.07.26)

--- ClasWork_Filter_02.07.26.py
+++ ClasWork_Filter_02.07.26.py
@@ -12,40 +12,54 @@ import cv2
 import numpy as np
 from sympy.physics.units.definitions.unit_definitions import gauss
 
-# img = cv2.imread('data/lesson3/notes.png')
-#
-# img = cv2.resize(img, (600, 600))
-# cv2.imshow('original', img)
-#
-# gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('grey_image', gray_image)
-# #
-# # threshold = 128
-# #
-# # mask = gray_image < threshold
-# #
-# # gray_image[mask] = 0
-# # gray_image[~mask] = 255
-# # cv2.imshow('grey_image', grey_image)
-#
-#
-#
-# # ADAPTIVE
-# res = cv2.adaptiveThreshold(
-#     gauss,
-#         255,
-#     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-#     cv2.THRESH_BINARY,
-#     11,
-#     2,
-# )
-# cv2.imshow('res', res)
+img = cv2.imread('data/lesson3/notes.png')
 
+img = cv2.resize(img, (600, 600))
+cv2.imshow('original', img)
+
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray_image', gray_image)
+
+threshold = 128
+
+mask = gray_image < threshold
+
+gray_image[mask] = 0
+gray_image[~mask] = 255
+cv2.imshow('grey_image', gray_image)
+
+# GAUSE
+# gauss = cv2.GaussianBlur(
+#     gray_image,       # зображення з шумом
+#     (3, 3),     # розмір фільтру(ядра)
+#     sigmaX=0,      # наскільки важливими є далекі пікселі, adaptive -> value
+# )
+# cv2.imshow('gauss', gauss)
+
+# ДВОСТОРОННІЙ ФІЛЬТР
+bilat = cv2.bilateralFilter(
+    gray_image,         # зображення з шумом
+    d=5,                # розмір фільтру
+    sigmaColor=75,      # наскільки важливі пікселі іншого кольору
+    sigmaSpace=50,      # наскільки важливими є далекі пікселі
+)
+cv2.imshow("Bilateral Filter", bilat)
+
+# ADAPTIVE
+res = cv2.adaptiveThreshold(
+    bilat,         # зображення з текстом (чорно-біле)
+        255,    # білий колір
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,     # фільтр для обрахунку порогу (Гаус)
+    cv2.THRESH_BINARY,      # цей рядок просто треба вказаати
+    7,     # розмір фільтру
+    3,           # наскільки ПІКСЕЛЬ має відрізнятися від порогу
+)
+cv2.imshow('Adaptive', res)
 
 
 # Завдання 2
-# Відкрийте зображення data/lesson3/sudoku.jpg. Проведіть
-# для нього бінарізацію, а саме
+# Відкрийте зображення data/lesson3/sudoku.jpg.
+# Проведіть для нього бінарізацію, а саме
 #  CLAHE
 #  гаусове розмиття
 #  адаптивна бінарізація
@@ -54,16 +68,17 @@ from sympy.physics.units.definitions.unit_definitions import gauss
 # Порівняйте результати для гаусової та середньої адаптивної бінарізації
 
 sudoku_image = cv2.imread('data/lesson3/sudoku.jpg')
-cv2.imshow('sudoku', sudoku_image)
+cv2.imshow('sudoku_ORIGIN', sudoku_image)
 
 gray = cv2.cvtColor(sudoku_image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('gray', gray)
 
-
+# CLAHE
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 result = clahe.apply(gray)
+cv2.imshow('clahe', result)
 
-
+# Гаусове розмиття
 gauss = cv2.GaussianBlur(gray, (3, 3), 1.5)
 cv2.imshow('gauss', gauss)
 
@@ -75,7 +90,7 @@ res = cv2.adaptiveThreshold(
     11,
     2,
 )
-cv2.imshow('Adaptive Gauss', res)
+cv2.imshow('Adaptive + Gauss', res)
 
 result_gray = cv2.fastNlMeansDenoising(gray, None, h=10, templateWindowSize=7, searchWindowSize=21)
 cv2.imshow('NLMean', result_gray)
@@ -84,8 +99,7 @@ cv2.imshow('NLMean', result_gray)
 
 # Завдання 3
 # Використовуючи utils.trackbar_decorator Побудуйте class
-# ThresholdingParameterSelector для підбору параметрів для
-# адаптивної бінарізації.
+# ThresholdingParameterSelector для підбору параметрів для  адаптивної бінарізації.
 # Методи:
 # o run(img, **kwargs) – головний метод який запускає застосовує усі перетворення до
 # зображення і повертає маску.
