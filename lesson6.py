185b37995950a3e4396ee6205aa75dd847c94286
HalyshAnton <kyhar007@gmail.com>
2026-07-09 19:47:07 +0300

    lesson6

--- lesson6.py
+++ lesson6.py
@@ -1,97 +1,114 @@
 # детекція об'єктів(YOLO)
 
-import ultralytics
 import cv2
+import ultralytics
+from fontTools.varLib.instancer import names
 
 # створення моделі
-model = ultralytics.YOLO('yolov8s.pt')
+# s -- small(розмір моделі)
+model = ultralytics.YOLO("yolo11s.pt")
 
 # отримати зображення з ввідео
 cap = cv2.VideoCapture('data/lesson8/cars+bikes.mp4')
 success, img = cap.read()
-
 img = cv2.resize(img, None, fx=0.5, fy=0.5)
+print(img.shape)
 
-cv2.imshow('original', img)
+cv2.imshow("orig", img)
+
 
 # застосування моделі
-# predict може отримувати декілька зображень за раз
-# predict([img1, img2, img3]) і тому results це список
-# [result1, result2, result3]
-
+# модель може одночасно обробити декілька зображень [img1, img2, img3, ..]
+# на виході results -- список результів [result1, result2, result3, ..]
 results = model.predict(
-    img,    # саме зображення
-    device='cpu',    # процесор де запускати модель
-                     # cpu -- звичайний процес
-                     # cuda -- графічний процесор
-    conf=0.25,       # мінімальній відсоток для об'єкта щоб попасти в result
-    iou=0.7,        # максимально можливий рівень перетину рамок(якщо більше то вважаємо
-                    # що це рамки для одного й того самого об'єкта)
-    classes=[0, 1]  # id класів які детектити
-)
+    img,  # зображення
+    device="cuda",  # процесор де робити обчисдення
+                # cpu --звичайний процесор
+                # cuda -- графічний процесор(gpu) на Windows\Linux
+                # mps -- графічний процесор(gpu) на MacOS
 
+    conf=0.25,   # мінімальна ймовірність для об'єктів,
+                # все що менше відсіюється
+
+    iou=0.5,   # наскільки сильно можуть перетинаться рамки,
+               # якщорамки перетинаються сильніше то залишаєму ту
+                # в якої більшо ймовірність
+
+    #classes=[0, 1],  # класи які враховувати(див result.names)
+)
+# print(type(results))
+# print(results)
 
 # results -- список з одним елементом
+# отримати результ
 result = results[0]
 
+
+
 # отримати назви класів(об'єкти які вмієме визначати модель)
-names = result.names # dict --- id: str
+names = result.names
+print(type(names))
 print(names)
 
+
 # самі об'єкти
-cls = result.boxes.cls
-print(cls)
+boxes = result.boxes
+# print(type(boxes))
+# print(boxes)
+
 
 # візуалізація результів
 res_img = result.plot()
-cv2.imshow('result', res_img)
-
+cv2.imshow("result", res_img)
 
 # ймовірності
-conf = result.boxes.conf
+conf = boxes.conf
+print(type(conf))
+
+# відклюсти від графічного процесора
+conf = conf.cpu()
+
+# перевести в масив numpy
+conf = conf.numpy()
+
 print(conf)
+print(conf.shape)
+print(conf.dtype)
 
 
 # рамка(box)
-boxes = result.boxes
+box = boxes[0]  # дані першого обєкта
+
+print(box.conf)
+print(box.cls)  # індекс класу
+print(box.xyxy)  # координати меж
+
+
+# вивести назву та ймовірність
+conf = box.conf
+conf = conf.cpu().numpy()
+print(f"Ймовірність першого обєкта {conf[0]}")
+
+cls = box.cls
+cls = cls.cpu().numpy()
+print(f"Індекс класу першого обєкта {cls[0]}")
+
+class_id = int(cls[0])
+class_name = names[class_id]
+print(f"Клас першого обєкта {class_name}")
 
 # отримати перший об'єкт
-box = boxes[0]
 
-print(box)
 
 # координати
-xyxy = box.xyxy[0]
 
 # переведення координат в int
-x1, y1, x2, y2 = map(int, xyxy)
+
+
 
 # вирізати об'єк з всього зображення
-# y -- відповідають за рядки
-# x -- відповідають за стовпчики
-roi = img[y1:y2, x1:x2]
-
-cv2.imshow('object', roi)
 
 
 # # відео
-# while True:
-#     success, img = cap.read()
-#
-#     if not success:
-#         break
-#
-#     img = cv2.resize(img, None, fx=0.5, fy=0.5)
-#
-#     results = model.predict(img)
-#     result = results[0]
-#
-#     res_img = result.plot()
-#
-#     cv2.imshow('result', res_img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
 
 cv2.waitKey(0)
