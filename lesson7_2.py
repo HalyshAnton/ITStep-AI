11c295587a29af0b86bfc6ad303b630c1a11ad0e
HalyshAnton <kyhar007@gmail.com>
2026-07-19 21:35:38 +0300

    lesson7_2

--- lesson7_2.py
+++ lesson7_2.py
@@ -1,6 +1,7 @@
-# from ultralytics import YOLO
-# import numpy as np
-# import cv2
+import ultralytics
+from ultralytics import YOLO
+import numpy as np
+import cv2
 #
 # # модель для сегментації
 # model = YOLO("yolo11s-seg.pt")
@@ -56,3 +57,66 @@
 #
 #
 # cv2.waitKey(0)
+
+
+# model = YOLO("data/lesson_seg/crop-seg.pt")
+# img = cv2.imread("data/lesson_seg/crop3.jpg")
+#
+#
+# results = model.predict(
+#     img,
+#     device="cuda",
+# )
+# result = results[0]
+#
+# masks = result.masks
+# masks_data = masks.data
+#
+# masks_data = masks_data.cpu().numpy()
+#
+# # знайди площу(суму) кожнох маски
+# masks_area = masks_data.sum(axis=(1, 2))
+#
+# # індекс найбільшої площі
+# i = masks_area.argmax()
+#
+#
+# print(masks_area)
+# print(i)
+
+
+
+# тренування моделі
+model = ultralytics.YOLO("yolo11s.pt")
+
+model.train(
+    data="data/yolo_dataset/dataset.yaml",
+    device="cuda",
+    batch=6,   # кількість зображень які модель бічить за 1 раз(розмір порції)
+    hsv_h=0.7
+)
+
+
+# використання натренованої нефромережі
+
+model = YOLO("runs/detect/train2/weights/best.pt")
+
+results = model.predict(
+    "data/yolo_dataset/images/train/0000068_03388_d_0000010.jpg",
+    device="cuda"
+)
+result = results[0]
+res = result.plot()
+
+cv2.imshow("train", res)
+
+results = model.predict(
+    "data/yolo_dataset/images/val/0000023_00868_d_0000010.jpg",
+    device="cuda"
+)
+result = results[0]
+res = result.plot()
+
+cv2.imshow("val", res)
+cv2.waitKey(0)
+
